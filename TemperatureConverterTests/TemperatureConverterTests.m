43adce9bc65ff80860b8194d3d442b438cbfa708
Vasanthy Kolluri <vasanthykolluri@gmail.com>
2014-01-12 18:25:39 -0800

    Initial Commit

---
+++ TemperatureConverterTests/TemperatureConverterTests.m
@@ -0,0 +1,34 @@
+//
+//  TemperatureConverterTests.m
+//  TemperatureConverterTests
+//
+//  Created by Vasanthy Kolluri on 1/12/14.
+//  Copyright (c) 2014 Vasanthy Kolluri. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TemperatureConverterTests : XCTestCase
+
+@end
+
+@implementation TemperatureConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
